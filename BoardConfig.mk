70c9471b59a415d25a72ded6cca6de187eb7ac6f
twinnfamous <twinnfamous@gmail.com>
2019-05-18 21:23:23 -0700

    fix for decryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,16 +25,17 @@
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a75
+TARGET_CPU_VARIANT := kryo
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a75
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
 
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
@@ -45,11 +46,15 @@ TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=eng
 BOARD_KERNEL_CMDLINE += skip_override androidboot.fastboot=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_PREBUILT_KERNEL := device/oneplus/guacamole/prebuilt/Image.gz-dtb
+
+# Putting here just in case i need them later
+#BOARD_HEADER_VERSION 1
+#BOARD_HEADER_SIZE 1648
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
@@ -122,10 +127,14 @@ AB_OTA_PARTITIONS += \
     dtbo 
 
 # Encryption
+# Custom Platform Version and Security Patch
+# Must match build.prop of current system for decryption to work properly!
+PLATFORM_VERSION := 9
+# 9.0.5 STOCK
 PLATFORM_SECURITY_PATCH := 2025-12-31
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
-TW_INCLUDE_FBE_METADATA_DECRYPT
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
 BOARD_USES_METADATA_PARTITION := true
 
 # Extras
@@ -136,5 +145,13 @@ RECOVERY_INSTALLER_PATH := device/oneplus/guacamole/installer
 TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_HAS_EDL_MODE := true
-TWRP_INCLUDE_LOGCAT := true
+
+# TWRP Debug Flags
+#TWRP_EVENT_LOGGING := true
 TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+#TARGET_RECOVERY_DEVICE_MODULES += debuggerd # strace
+#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(OUT)/system/bin/debuggerd # $(OUT)/system/xbin/strace
+#TARGET_RECOVERY_DEVICE_MODULES += twrpdec
+#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_RECOVERY_ROOT_OUT)/sbin/twrpdec
+#TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,3 @@
+export LC_ALL=C
+export ALLOW_MISSING_DEPENDENCIES=true
 add_lunch_combo omni_guacamole-eng
